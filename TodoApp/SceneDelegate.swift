ad466573a1b80b57354bb0b75c45fd6fa00e5578
yukifox <huytran12596@gmail.com>
2020-07-31 15:03:10 +0700

    Create Todo App

--- TodoApp/SceneDelegate.swift
+++ TodoApp/SceneDelegate.swift
@@ -17,7 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowSence = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowSence.coordinateSpace.bounds)
+        window?.windowScene = windowSence
+        let mainVC = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
